9ffa4c7227eec9a47809d27156af253666508a0e
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-10-20 13:27:08 +0200

    renamed files

--- MeetsyAPI/Program.cs
+++ MeetsyAPI/Program.cs
@@ -1,19 +1,12 @@
 using MeetsyAPI;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
-using MongoDB.Driver.Linq;
-using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
